324a89f9376b7bc846c6a5e04889ab49f98c7b86
Dewangga <hamadakinose@gmail.com>
2024-04-03 23:57:38 +0700

    added cors for documentation API

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors")
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.set('view engine', 'ejs');
+app.use(cors())
 
 const v1Router = require('./routes/v1/index.js')
 app.use('/v1', v1Router)
